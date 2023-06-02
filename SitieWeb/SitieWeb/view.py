18ec0db7ee0904c229d48898852d2b793d730d4a
sairaf3557 <sairafbrenda25@gmail.com>
2023-06-01 20:03:02 -0600

    "modificaciones"

--- SitieWeb/SitieWeb/view.py
+++ SitieWeb/SitieWeb/view.py
@@ -1,15 +1,20 @@
 import datetime
 from django.http import HttpResponse
 from django.template import Template,Context
-def saludo(request):
 
- nombre="Angel"
+class Persona(object):
+ def __init__(self,nombre,apellido):
+  self.nombre=nombre
+  self.apellido=apellido
+def saludo(request):
+ P2=Persona("julio","santos")
+
  now=datetime.datetime.now()
 
  doc_ex=open("C:/Users/sairaf/Desktop/web/WebDjangoNew/SitieWeb/SitieWeb/Plantillas/miplantilla.html")
  ptl=Template(doc_ex.read())
  doc_ex.close()
- ctx=Context({"nombre_persona":nombre,"momento_actual":now})
+ ctx=Context({"nombre_persona":P2.nombre,"apellido_persona":P2.apellido,"momento_actual":now, "temas":["pantillas","metodos"]})
  documento=ptl.render(ctx)
  return HttpResponse(documento)
 
